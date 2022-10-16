08816479df32823dfd223da52c7e0c3d38b7f200
marcelbittrich <marcel.bittrich@gmail.com>
2022-10-16 13:35:38 +0200

    Added performance assessment

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,13 @@
 
 Game *game = nullptr;
 
+// Performance assessment
+bool showUpdateTime = false;
+bool showRenderTime = false;
+bool showFrameTime = false;
+bool showDelayedFrameTime = false;
+bool showFPS = true;
+
 int main(int argc,char * argv[])
 {
 
@@ -19,8 +26,10 @@ int main(int argc,char * argv[])
 		game->frameStart = SDL_GetTicks();
 
 		game->handleEvents();
-		game->update();
+		game->update(); 
+		Uint32 updateTime = SDL_GetTicks() - game->frameStart;
 		game->render();
+		Uint32 renderTime = SDL_GetTicks() - updateTime - game->frameStart;
 
 		game->frameTime = SDL_GetTicks() - game->frameStart;
 
@@ -28,6 +37,14 @@ int main(int argc,char * argv[])
 		{
 			SDL_Delay((int)(frameDelay - game->frameTime));
 		}
+		Uint32 delayedFrameTime = SDL_GetTicks() - game->frameStart;
+
+		if (showUpdateTime) std::cout << "Update Time: " << updateTime << " ";
+		if (showRenderTime) std::cout << "Render Time: " << renderTime << " ";
+		if (showFrameTime) std::cout << "Frame Time: " << game->frameTime << " ";
+		if (showDelayedFrameTime) std::cout << "Delayed Frame Time: " << delayedFrameTime << " ";
+		if (showFPS) std::cout << "FPS: " << 1000/delayedFrameTime << " ";
+ 		if (showUpdateTime || showRenderTime|| showFrameTime || showDelayedFrameTime || showFPS) std::cout << std::endl;
 	};
 	
 	game->clean();
