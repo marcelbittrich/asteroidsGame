723ffb7c8057c225cf4c3326325e06eb59736d1e
marcelbittrich <marcel.bittrich@gmail.com>
2023-01-26 13:25:41 +0100

    refactor background points

--- src/background.cpp
+++ src/background.cpp
@@ -1,83 +1,98 @@
 #include "background.hpp"
 
-bgPoint::bgPoint()
+backgroundPoint::backgroundPoint()
 {
 
 }
 
-bgPoint::bgPoint(int xPos, int yPos)
+backgroundPoint::backgroundPoint(int xPos, int yPos)
 {
     this->xPos = xPos;
     this->yPos = yPos;
+
     originPos.x = xPos;
     originPos.y = yPos;
+
     renderPos.x = originPos.x;
     renderPos.y = originPos.y;
+
     color[0] = rand() %255;
     color[1] = rand() %255;
     color[2] = rand() %255;
     color[3] = 255;
+
     onOrigin = true;
 }
 
-void bgPoint::returnToOrigin(float *deltaTime){
-    float minVelocity = 0.5;
-    float steepness = 0.005;
-    float squareDistance, distance; 
-    squareDistance = (originPos.x-xPos) * (originPos.x-xPos) + (originPos.y-yPos) * (originPos.y-yPos);
-    distance = SDL_sqrtf(squareDistance);
+void backgroundPoint::returnToOrigin(float *deltaTime)
+{
+    squareDistanceToOrigin = (originPos.x-xPos) * (originPos.x-xPos) + (originPos.y-yPos) * (originPos.y-yPos);
+    distanceToOrigin = SDL_sqrtf(squareDistanceToOrigin);
 
-    vDistance[0] = (originPos.x-xPos);
-    vDistance[1] = (originPos.y-yPos);
-    nDistance[0] = (vDistance[0]/(distance));
-    nDistance[1] = (vDistance[1]/(distance));
-    float distanceDependentVelocity = steepness * distance * distance;
+    vectorDistance[0] = (originPos.x-xPos);
+    vectorDistance[1] = (originPos.y-yPos);
+    normalizedDistance[0] = (vectorDistance[0]/(distanceToOrigin));
+    normalizedDistance[1] = (vectorDistance[1]/(distanceToOrigin));
 
-    float getBackVelocity;
-    getBackVelocity = std::max(minVelocity,distanceDependentVelocity) * *deltaTime * 60;
+    float distanceDependentVelocity = distanceVelocityFunctionSteepness * distanceToOrigin * distanceToOrigin;
 
-    vChange[0] = (nDistance[0] * getBackVelocity);
-    vChange[1] = (nDistance[1] * getBackVelocity);
+    float returnToOriginVelocity;
+    returnToOriginVelocity = std::max(minReturnVelocity,distanceDependentVelocity) * *deltaTime * 60;
+
+    vectorChange[0] = (normalizedDistance[0] * returnToOriginVelocity);
+    vectorChange[1] = (normalizedDistance[1] * returnToOriginVelocity);
     
-    if (vChange[0] * vChange[0] + vChange[1] * vChange[1] > squareDistance)
+    //if point will be cloase to origin, put it on origin
+    if (vectorChange[0] * vectorChange[0] + vectorChange[1] * vectorChange[1] > squareDistanceToOrigin)
     {
         xPos = originPos.x; 
         yPos = originPos.y;
+
         renderPos.x = originPos.x;
         renderPos.y = originPos.y;
-        onOrigin = true;
-    }   else {
-        xPos += vChange[0];
-        yPos += vChange[1];
+
+        onOrigin = true;       
+    }   
+    else 
+    {
+        xPos += vectorChange[0];
+        yPos += vectorChange[1];
+
         renderPos.x = round(xPos);
         renderPos.y = round(yPos);
+
         onOrigin = false;
     }
 }
 
-void bgPoint::update(GameObject colObject)
+void backgroundPoint::update(GameObject colObject)
 {   
+    //AABB collision check
     if (xPos > colObject.midPos[0] - colObject.colRadius 
     && xPos < colObject.midPos[0] + colObject.colRadius
     && yPos > colObject.midPos[1] - colObject.colRadius
     && yPos < colObject.midPos[1] + colObject.colRadius)
     {
-        squareDistance = (colObject.midPos[0] - xPos) * (colObject.midPos[0] - xPos) + (colObject.midPos[1] - yPos) * (colObject.midPos[1] - yPos);
-        squareColRadius = colObject.colRadius * colObject.colRadius;
-        float distance = SDL_sqrtf(squareDistance);
+        //distance based collision check
+        float squareDistanceToObject = (colObject.midPos[0] - xPos) * (colObject.midPos[0] - xPos) 
+                                    + (colObject.midPos[1] - yPos) * (colObject.midPos[1] - yPos);
+        float squareObjectCollisonRadius = colObject.colRadius * colObject.colRadius;
 
-        if (squareDistance <= squareColRadius)
+        if (squareDistanceToObject <= squareObjectCollisonRadius)
         {
+            float distanceToObject = SDL_sqrtf(squareDistanceToObject);
+
             float xDist = xPos - colObject.midPos[0];
             float yDist = yPos - colObject.midPos[1];
-            vDistance[0] = xDist;
-            vDistance[1] = yDist;
 
-            nDistance[0] = vDistance[0]/distance;
-            nDistance[1] = vDistance[1]/distance;
+            vectorDistance[0] = xDist;
+            vectorDistance[1] = yDist;
 
-            xPos = colObject.midPos[0] + nDistance[0] * colObject.colRadius;
-            yPos = colObject.midPos[1] + nDistance[1] * colObject.colRadius;
+            normalizedDistance[0] = vectorDistance[0]/distanceToObject;
+            normalizedDistance[1] = vectorDistance[1]/distanceToObject;
+
+            xPos = colObject.midPos[0] + normalizedDistance[0] * colObject.colRadius;
+            yPos = colObject.midPos[1] + normalizedDistance[1] * colObject.colRadius;
 
             renderPos.x = round(xPos);
             renderPos.y = round(yPos);
@@ -86,17 +101,26 @@ void bgPoint::update(GameObject colObject)
     }
 }
 
-void bgPoint::render(SDL_Renderer *renderer)
+void backgroundPoint::render(SDL_Renderer *renderer)
 {
-    float pointSizeScale = 1.5f;
-    int logicWidth = 0;
-    int logicHeight = 0;
+    //get current logic render size from renderer
+    int logicWidth, logicHeight;
     SDL_RenderGetLogicalSize(renderer, &logicWidth, &logicHeight);
-    SDL_RenderSetLogicalSize(renderer, round(logicWidth/pointSizeScale), round(logicHeight/pointSizeScale));
+
+    //get bigger background points by lowering the logic renderer size
+    int newlogiclogicWidth =    round(logicWidth/pointSizeScale); 
+    int newlogicHeight =        round(logicHeight/pointSizeScale);
+    SDL_RenderSetLogicalSize(renderer, newlogiclogicWidth, newlogicHeight);
+
+    //set the point render position according to the new logic render size
     int scaledPosX = round(renderPos.x/pointSizeScale); 
     int scaledPosY = round(renderPos.y/pointSizeScale); 
+
+    //render points
     SDL_SetRenderDrawColor(renderer, color[0], color[1], color[2], color[3]);
     SDL_RenderDrawPoint(renderer,scaledPosX,scaledPosY);
+
+    //reset logic render size
     SDL_RenderSetLogicalSize(renderer, logicWidth, logicHeight);
 }
 
@@ -114,16 +138,19 @@ background::background(int windowWidth, int windowHeight, int divider)
     for (int i = 0; i < divider; i++)
     {
         int xPos = width/(float)divider * i + width/(float)divider/2.0f;
-        std::vector<bgPoint> bgPointColumn;
+        std::vector<backgroundPoint> backgroundPointColumn;
         for (int j = 0; j < divider; j++)
         {
             int yPos = height/(float)divider * j + height/(float)divider/2.0f;
-            bgPoint newPoint(xPos,yPos); 
-            bgPointColumn.push_back(newPoint);
+            backgroundPoint newPoint(xPos,yPos); 
+            backgroundPointColumn.push_back(newPoint);
         }
-        pointCloud.push_back(bgPointColumn);
+        pointCloud.push_back(backgroundPointColumn);
     }  
 }
+
+//void createBackgroundPoints()
+//{}
 
 void background::update(std::list<GameObject>colObjects, float *updateTime)
 {      
@@ -158,9 +185,9 @@ void background::update(std::list<GameObject>colObjects, float *updateTime)
         }
     }
 
-    for (std::vector<bgPoint> &bgPointColumn : pointCloud)
+    for (std::vector<backgroundPoint> &backgroundPointColumn : pointCloud)
     {
-        for (bgPoint &singleBgPoint : bgPointColumn)
+        for (backgroundPoint &singleBgPoint : backgroundPointColumn)
             if (!singleBgPoint.onOrigin)
             {   
                 singleBgPoint.returnToOrigin(updateTime);
@@ -173,13 +200,15 @@ void background::update(std::list<GameObject>colObjects, float *updateTime)
 
 void background::render(SDL_Renderer *renderer)
 {
-    for (std::vector<bgPoint> &bgPointColumn : pointCloud)
+    for (std::vector<backgroundPoint> &backgroundPointColumn : pointCloud)
     {
-        for (bgPoint &singleBgPoint : bgPointColumn)
-            if (!singleBgPoint.onOrigin)
+        for (backgroundPoint &singleBackgroundPoint : backgroundPointColumn)
+        {
+            if (!singleBackgroundPoint.onOrigin)
             {   
-                singleBgPoint.render(renderer);
-            }    
+                singleBackgroundPoint.render(renderer);
+            }  
+        }  
     }
 }
 
--- src/background.hpp
+++ src/background.hpp
@@ -7,24 +7,34 @@
 #include "gameobjects.hpp"
 
 
-class bgPoint
+class backgroundPoint
 {
 private:
+
     Uint8 color [4];
+
 public:
-    float xPos, yPos;
-    bool onOrigin;
+
+    float   xPos, yPos;
+    bool    onOrigin;
+
     SDL_Point originPos;
     SDL_Point renderPos;
-    bgPoint();
-    bgPoint(int xPos, int yPos);
-    
-    std::vector<float> vDistance = {0.0f, 0.0f};
-    std::vector<float> nDistance = {0.0f, 0.0f};
-    std::vector<float> vChange = {0.0f, 0.0f};
 
-    float squareDistance; 
-    float squareColRadius;
+    float vectorDistance [2]        = {0.0f, 0.0f};
+    float normalizedDistance [2]    = {0.0f, 0.0f};
+    float vectorChange [2]          = {0.0f, 0.0f};
+
+    //return function values
+    float squareDistanceToOrigin, distanceToOrigin;
+    float minReturnVelocity                 = 0.5;
+    float distanceVelocityFunctionSteepness = 0.005;
+
+    //render values
+    float pointSizeScale = 1.5;
+
+    backgroundPoint();
+    backgroundPoint(int xPos, int yPos);
 
     void update(GameObject colObject);
     void render(SDL_Renderer *renderer);
@@ -34,11 +44,15 @@ public:
 class background
 {
 private:
+
     int width, height, divider;
-    std::vector<std::vector<bgPoint>>pointCloud;
+    std::vector<std::vector<backgroundPoint>>pointCloud;
+
 public:
+
     background();
     background(int windowWidth, int windowHeight, int divider);
+
     void update(std::list<GameObject>colObjects, float *deltaTime);
     void render(SDL_Renderer *renderer);
 
